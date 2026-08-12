7e535f82f0760907ba7172fdb146d438d965960f
mahesh15ai <gomaskarm@gmail.com>
2026-08-12 15:33:54 +0530

    Added media routes for Vercel production

--- CEM/urls.py
+++ CEM/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.shortcuts import redirect
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
-    # Root URL Redirection (मोबाईलवर IP टाकल्यास थेट Login पेज उघडेल)
+    # Root URL Redirection
     path('', lambda request: redirect('/accounts/login/')),
     
     path('admin/', admin.site.urls),
@@ -14,8 +14,8 @@ urlpatterns = [
     path('attendance/', include('attendance.urls')),
     path('events/', include('events.urls')),
     path('certificates/', include('certificates.urls')),
+    
+    # 📌 Production (Vercel) वर Media Files आणि Static Files सर्व्ह करण्यासाठी:
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
